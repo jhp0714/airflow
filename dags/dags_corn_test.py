605de7dfac0175568abc68b0af7266f4a59acbc2
jhp0714 <gowns171@naver.com>
2024-04-07 20:36:15 +0900

    커넥션 테스트용 dag 업로드

--- dags/dags_corn_test.py
+++ dags/dags_corn_test.py
@@ -4,11 +4,11 @@ import datetime
 from airflow.operators.empty import EmptyOperator
 
 with DAG(
-    dag_id = "dags_corn_test.py",
+    dag_id = "dags_corn_test",
     schedule = None,
-    start_date = pendulum.datetime(2023, 3, 1 tz="Asia/Seoul"),
+    start_date = pendulum.datetime(2023, 3, 1, tz="Asia/Seoul"),
     catchup = False
- ) as dag:
+ ) as dag :
 
     t1 = EmptyOperator(
         task_id = "t1"
