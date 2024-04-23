f82e3c8f24b4dbb6d26bbc45bf088e55e4cb3b70
jhp0714 <gowns171@naver.com>
2024-04-23 22:27:53 +0900

    이제 좀 되라

--- dags/dags_bash_with_template.py
+++ dags/dags_bash_with_template.py
@@ -4,13 +4,15 @@ from airflow.operators.bash import BashOperator
 
 with DAG(
     dag_id="dags_bash_with_template",
-    schedule= pendulum.datetime(2023, 3, 1, tz="Asia/Seoul"),
+    schedule= "10 0 * * *",
+    start_date = pendulum.datetime(2023, 3, 1, tz="Asia/Seoul"),
     catchup=False
 ) as dag:    
     bash_t1 = BashOperator(
         task_id='bash_t1',
         bahs_command='echo "End date is {{ data_interval_end }}"'
     )
+    
     bash_t2 = BashOperator(
         task_id='bash_t2',
         env={
