14d6735c52a6e7eca1daf16e71a16290c4a02709
jhp0714 <gowns171@naver.com>
2024-04-23 22:18:49 +0900

    오류 수정

---
+++ dags/dags_bash_with_template.py
@@ -0,0 +1,23 @@
+from airflow import DAG
+import pendulum
+from airflow.operators.bash import BashOperator
+
+with DAG(
+    dag_id="dags_bash_with_template",
+    schedule= pendulum.datetime(2023, 3, 1, tz="Asia/Seoul"),
+    catchup=False
+) as dag:    
+    bash_t1 = BashOperator(
+        task_id='bash_t1',
+        bahs_command='echo "End date is {{ data_interval_end }}"'
+    )
+    bash_t2 = BashOperator(
+        task_id='bash_t2',
+        env={
+            'START_DATE' : '{{data_interval_start | ds}}',
+            'END_DATE' : '{{data_interval_end}}'
+        },
+        bahs_command='echo $START_DATE && echo $END_DATE'
+    )
+
+    bash_t1 >> bash_t2
