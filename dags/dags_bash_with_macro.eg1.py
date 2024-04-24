035ac28738569beb7fb05490ecea6b7bd13bb527
jhp0714 <gowns171@naver.com>
2024-04-25 00:17:31 +0900

    dags_bash_with_macro_eg 업로드

---
+++ dags/dags_bash_with_macro.eg1.py
@@ -0,0 +1,17 @@
+from airflow import DAG
+import pendulum
+from airflow.operators.bash import BashOperator
+
+with DAG(
+    dag_id="dags_bash_with_macro.eg1",
+    schedule="10 0 L * * ",
+    start_date=pendulum.datetime(2023, 3, 1, tz="Asia/Seoul"),
+    catchup=False
+) as dag:
+    # START_DATE : 전월 말일, END_DATE : 1일 전
+    bash_task_1 = BashOperator(
+        task_id='bash_task_1',
+        env={'START_DATE':'{{dtat_interval_start.in_timezone("Asia/Seoul") | ds}}',
+             'END_DATE':'{{dtat_interval_end.in_timezone("Asia/Seoul") - macros.dateutil.relativedelta.relativedelta(days=1) | ds}}'},
+        bash_command='echo "START_DATE : #START_DATE" && echo "END_DATE : $END_DATE"'
+    )
