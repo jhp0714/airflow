e2a091e00fdd788c0f8121c6ed6ad4709e0e1751
jhp0714 <gowns171@naver.com>
2024-04-25 00:34:36 +0900

    aa

--- dags/dags_bash_with_macro.eg1.py
+++ dags/dags_bash_with_macro.eg1.py
@@ -4,14 +4,14 @@ from airflow.operators.bash import BashOperator
 
 with DAG(
     dag_id="dags_bash_with_macro.eg1",
-    schedule="10 0 L * * ",
+    schedule="10 0 L * *",
     start_date=pendulum.datetime(2023, 3, 1, tz="Asia/Seoul"),
     catchup=False
 ) as dag:
     # START_DATE : 전월 말일, END_DATE : 1일 전
     bash_task_1 = BashOperator(
         task_id='bash_task_1',
-        env={'START_DATE':'{{data_interval_start.in_timezone("Asia/Seoul") | ds}}',
-             'END_DATE':'{{data_interval_end.in_timezone("Asia/Seoul") - macros.dateutil.relativedelta.relativedelta(days=1).strftime("%Y-%m-%d")}}'},
+        env={'START_DATE':'{{ data_interval_start.in_timezone("Asia/Seoul") | ds }}',
+             'END_DATE':'{{ (data_interval_end.in_timezone("Asia/Seoul") - macros.dateutil.relativedelta.relativedelta(days=1)) | ds}}'},
         bash_command='echo "START_DATE : $START_DATE" && echo "END_DATE : $END_DATE"' 
     )
