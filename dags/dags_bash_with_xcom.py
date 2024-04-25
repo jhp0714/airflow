cd38a82c4f6c4112cf4277d32ccd234c6038d006
jhp0714 <gowns171@naver.com>
2024-04-25 21:40:13 +0900

    'd'

--- dags/dags_bash_with_xcom.py
+++ dags/dags_bash_with_xcom.py
@@ -3,7 +3,7 @@ import pendulum
 from airflow.operators.bash import BashOperator
 
 with DAG(
-    dag_id="dags_python_with_xcom_eg1",
+    dag_id="dags_bash_with_xcom",
     schedule="10 0 * * *",
     start_date=pendulum.datetime(2023, 3, 1, tz="Asia/Seoul"),
     catchup=False
