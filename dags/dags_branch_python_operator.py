523e7aefd9b8e36aa3da47de593e106da3be50cb
jhp0714 <gowns171@naver.com>
2024-05-02 20:38:51 +0900

    df

--- dags/dags_branch_python_operator.py
+++ dags/dags_branch_python_operator.py
@@ -4,7 +4,7 @@ from airflow.operators.python import PythonOperator
 from airflow.operators.python import BranchPythonOperator
 
 with DAG(
-    dag_id="dags_bash_with_variable",
+    dag_id="dags_branch_python_operator",
     schedule="10 9 * * *",
     start_date=pendulum.datetime(2023,3,1,tz="Asia/Seoul"),
     catchup=False
