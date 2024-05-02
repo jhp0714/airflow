9d3871b2d94c03b8f52956c13fdd847aa9e6c2fe
jhp0714 <gowns171@naver.com>
2024-05-02 19:25:34 +0900

    df

--- dags/dags_bash_with_variable.py
+++ dags/dags_bash_with_variable.py
@@ -1,7 +1,7 @@
 from airflow import DAG
 import pendulum
 from airflow.operators.bash import BashOperator
-from airflow.models import variable
+from airflow.models import Variable
 
 with DAG(
     dag_id="dags_bash_with_variable",
@@ -9,7 +9,7 @@ with DAG(
     start_date=pendulum.datetime(2023,3,1,tz="Asia/Seoul"),
     catchup=False
 ) as dag:
-    var_value = variable.get("sample_key")
+    var_value = Variable.get("sample_key")
 
     bash_var_1 = BashOperator(
         task_id="bash_var_1",
