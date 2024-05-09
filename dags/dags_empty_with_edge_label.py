779f89e38a383d6d85cc0fdfb94630508865e8bf
jhp0714 <gowns171@naver.com>
2024-05-09 22:20:59 +0900

    df

--- dags/dags_empty_with_edge_label.py
+++ dags/dags_empty_with_edge_label.py
@@ -4,36 +4,36 @@ from airflow.operators.empty import EmptyOperator
 from airflow.utils.edgemodifier import Label
 
 with DAG(
-    dag_id='dags_empty_with_dege_label',
+    dag_id='dags_empty_with_edge_label',
     start_date=pendulum.datetime(2024,4,1, tz='Asia/Seoul'),
     schedule=None,
     catchup=False
 ) as dag:
     
     empty_1 = EmptyOperator(
-        task_id='empty_1'
+        taks_id='empty_1'
     )
 
     empty_2 = EmptyOperator(
-        task_id='empty_2'
+        taks_id='empty_2'
     )
 
     empty_1 >> Label('between 1 and 2') >> empty_2
 
     empty_3 = EmptyOperator(
-        task_id='empty_3'
+        taks_id='empty_3'
     )
 
     empty_4 = EmptyOperator(
-        task_id='empty_4'
+        taks_id='empty_4'
     )
 
     empty_5 = EmptyOperator(
-        task_id='empty_5'
+        taks_id='empty_5'
     )
 
     empty_6 = EmptyOperator(
-        task_id='empty_6'
+        taks_id='empty_6'
     )
 
-    empty_2 >> Label('Start Branch') >> ['empty_3', 'empty_4', 'empty_5'] >> Label('End Branch') >> empty_6
+    empty_2 >> Label('Start Branch') >> [empty_3, empty_4, empty_5] >> Label('End Branch') >> empty_6
