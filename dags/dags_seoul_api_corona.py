4f8aa790e6ee5f944a101ceadc0895953bd7be14
jhp0714 <gowns171@naver.com>
2024-05-19 20:43:56 +0900

    df

--- dags/dags_seoul_api_corona.py
+++ dags/dags_seoul_api_corona.py
@@ -3,24 +3,22 @@ from airflow import DAG
 import pendulum
 
 with DAG(
-    dag_id = 'dags_seoul_api_corona',
+    dag_id='dags_seoul_api_corona',
     schedule='0 7 * * *',
-    start_date=pendulum.datetime(2024,4,1, tz='Asia/Seoul'),
+    start_date=pendulum.datetime(2023,4,1, tz='Asia/Seoul'),
     catchup=False
 ) as dag:
     '''서울시 코로나19 확진자 발생동향'''
-
     tb_corona19_count_status = SeoulApiToCsvOperator(
-        task_id = 'tb_corona19_count_status',
+        task_id='tb_corona19_count_status',
         dataset_nm='TbCorona19CountStatus',
         path='/opt/airflow/files/TbCorona19CountStatus/{{data_interval_end.in_timezone("Asia/Seoul") | ds_nodash }}',
         file_name='TbCorona19CountStatus.csv'
     )
-
-    '''서울시 코로나19 백신 예방 접종 현황'''
-
+    
+    '''서울시 코로나19 백신 예방접종 현황'''
     tv_corona19_vaccine_stat_new = SeoulApiToCsvOperator(
-        task_id = 'tv_corona19_vaccine_stat_new',
+        task_id='tv_corona19_vaccine_stat_new',
         dataset_nm='tvCorona19VaccinestatNew',
         path='/opt/airflow/files/tvCorona19VaccinestatNew/{{data_interval_end.in_timezone("Asia/Seoul") | ds_nodash }}',
         file_name='tvCorona19VaccinestatNew.csv'
--- plugins/operators/seoul_api_to_csv_operator.py
+++ plugins/operators/seoul_api_to_csv_operator.py
@@ -1,9 +1,9 @@
 from airflow.models.baseoperator import BaseOperator
 from airflow.hooks.base import BaseHook
-import pandas as pd
+import pandas as pd 
 
 class SeoulApiToCsvOperator(BaseOperator):
-    template_fields = ('endpoint','path','file_name','base_dt')
+    template_fields = ('endpoint', 'path','file_name','base_dt')
 
     def __init__(self, dataset_nm, path, file_name, base_dt=None, **kwargs):
         super().__init__(**kwargs)
@@ -15,6 +15,7 @@ class SeoulApiToCsvOperator(BaseOperator):
 
     def execute(self, context):
         import os
+        
         connection = BaseHook.get_connection(self.http_conn_id)
         self.base_url = f'http://{connection.host}:{connection.port}/{self.endpoint}'
 
@@ -26,25 +27,25 @@ class SeoulApiToCsvOperator(BaseOperator):
             self.log.info(f'끝:{end_row}')
             row_df = self._call_api(self.base_url, start_row, end_row)
             total_row_df = pd.concat([total_row_df, row_df])
-            if len(row_df) < 1000 :
+            if len(row_df) < 1000:
                 break
-            else :
+            else:
                 start_row = end_row + 1
                 end_row += 1000
 
-        if not os.path.exists(self.path) :
+        if not os.path.exists(self.path):
             os.system(f'mkdir -p {self.path}')
         total_row_df.to_csv(self.path + '/' + self.file_name, encoding='utf-8', index=False)
 
     def _call_api(self, base_url, start_row, end_row):
         import requests
-        import json
-        
-        headers = {'Content-Type' : 'application/json',
-                   'charset' : 'utf-8',
-                   'Accept' : '*/*',
+        import json 
+
+        headers = {'Content-Type': 'application/json',
+                   'charset': 'utf-8',
+                   'Accept': '*/*'
                    }
-        
+
         request_url = f'{base_url}/{start_row}/{end_row}/'
         if self.base_dt is not None:
             request_url = f'{base_url}/{start_row}/{end_row}/{self.base_dt}'
