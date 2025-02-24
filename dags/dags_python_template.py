1d5120c8cc86700bd025004dccab3152d9c751b5
jhp0714 <gowns171@naver.com>
2025-02-24 18:32:56 +0900

    dags_python_template.py upload

--- dags/dags_python_template.py
+++ dags/dags_python_template.py
@@ -4,9 +4,9 @@ from airflow.decorators import task
 import pendulum
 
 with DAG(
-    dag_id='dags_python_template.py',
+    dag_id='dags_python_template',
     schedule='30 9 * * *',
-    start_date=pendulum.datetime(2025,2,25, tz='Asia/Seoul'),
+    start_date=pendulum.datetime(2025,2,23, tz='Asia/Seoul'),
     catchup=False
 ) as dag:
 
