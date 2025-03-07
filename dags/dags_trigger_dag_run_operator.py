1b69960d1e11ff39d71e9ef3d6452c9deddc5c8d
jhp0714 <gowns171@naver.com>
2025-03-07 21:20:48 +0900

    dags_simple_http_operator upload

--- dags/dags_trigger_dag_run_operator.py
+++ dags/dags_trigger_dag_run_operator.py
@@ -19,7 +19,7 @@ with DAG(
         task_id='trigger_dag_task',
         trigger_dag_id='dags_python_operator',  # trigger 될 덱(실행시킬 덱)
         trigger_run_id=None,                    # Run_id 값 직접 지정
-        execution_date='{{ data_interval_end }}',# run_id를 manual__{{execution_date}}로 수행
+        execution_date='{{ data_interval_start }}',# run_id를 manual__{{execution_date}}로 수행
         reset_dag_run=True,                     # 이미 run_id 값이 있는 경우에도 재수행 여부 결정
         wait_for_completion=False,              # 트리거된 dag이 완료가 되어야 현재 task도 완료 상태가 되는지 여부
         poke_interval=60,                       # 트리거된 dag이 완료됬는지 확인하는 주기
